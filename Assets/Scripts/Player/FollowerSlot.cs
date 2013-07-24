a8fde3c29fe3c89a156f03272530aa28297c7377
FlowState <e.m.hibbs@gmail.com>
2013-07-24 12:22:24 +0100

    Commit on half-day

--- Assets/Scripts/Player/FollowerSlot.cs
+++ Assets/Scripts/Player/FollowerSlot.cs
@@ -7,13 +7,24 @@ public class FollowerSlot : MonoBehaviour
     public float Offset;
     private float sqrOffset;
     private Transform _transform, targetTransform, rotationTransform;
-    public Vector3 VectorOffset, VectorOffsetTwo, VectorOffsetThree;
+    public Vector3 VectorOffset;
     public float TransitionTime;
     private int currentOffset = 1;
     private Hashtable bounceTable;
     private Vector3 vectorOffset, desiredPosition;
     public KeyCode SwitchPosition;
     private bool doLerp = true;
+    public int CurrentPosition;
+    private GameObject _filler = null;
+
+    public GameObject Filler
+    {
+        get { return _filler; }
+        set { GetFiller(value); }
+    }
+
+    
+
     // Use this for initialization
 	void Start ()
 	{
@@ -30,49 +41,44 @@ public class FollowerSlot : MonoBehaviour
 	    rotationTransform = RotationMatch.transform;
 
 	}
-    
-    public void NextPattern()
-    {
-        switch (currentOffset)
-        {
-            case 1:
 
-                //currentOffset = 2;
-                vectorOffset = VectorOffsetTwo;
-                break;
-            case 2:
-                //currentOffset = 3;
-                vectorOffset = VectorOffsetThree;
-                break;
-            case 3:
-                //currentOffset = 1;
-                vectorOffset = VectorOffset;
-                break;
-            default:
-                break;
+    private void GetFiller(GameObject gObject)
+    {
+        if (_filler == null)
+        {
+            _filler = gObject;
+            _filler.transform.parent = _transform;
+            _filler.transform.position = _transform.position;
+            _filler.transform.rotation = _transform.rotation;
+        }
+        else
+        {
+            Debug.Log("Dont triple-stamp a double-stamp");
         }
 
-        currentOffset = (currentOffset%3) + 1;
     }
+
+    private void FillerGone()
+    {
+        SendMessageUpwards("FillerGone", CurrentPosition, SendMessageOptions.RequireReceiver);
+        _filler = null;
+    }
+
+    public void PopFiller()
+    {
+        _filler = null;
+    }
+
 
 	private void CalculatePosition()
 	{
-	    desiredPosition = targetTransform.TransformPoint(vectorOffset);
+	    desiredPosition = targetTransform.TransformPoint(VectorOffset);
         
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            Bounce();
-        }
-
-        if (Input.GetKeyDown(SwitchPosition) || Input.GetButtonDown("Formation") )
-        {
-            NextPattern();
-        }
 
         Rotate();
 
--- Assets/Scripts/Queue/QueueManager.cs
+++ Assets/Scripts/Queue/QueueManager.cs
@@ -1,53 +1,210 @@
+using System;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+
 public class QueueManager : MonoBehaviour
 {
     public enum QueueTypes
     {
-        LINE,
+        LINE = 0,
         BARRIER_FRONT,
+        PHALANX,
         CIRCLE
     }
 
 
 
     public int QueueSize = 3;
-    public List<FollowerSlot> followers;
+    public List<FollowerSlot> FollowerList;
     public GameObject FSPrefab;
     public QueueTypes currentQueueType = QueueTypes.LINE;
-    
+    private int sumQueueTypes;
+    private QueueTypes lastQueueType;
+    private int numOccupiedSlots;
+
+    public float LineZedOffset = -2f;
+
 	// Use this for initialization
-	void Start () {
-	    if (followers == null)
-	    {
-            followers = new List<FollowerSlot>();
-	        PopulateFollowers();
-	    }
+	void Start ()
+	{
+	    
+        SetUpQueueStuff();
+
+        //if (FollowerList == null)
+        //{
+        //    FollowerList = new List<FollowerSlot>();
+        //    PopulateFollowers();
+        //}
+
+	    InitializeFollowers();
 
 	}
 
-    
-
-    private void PopulateFollowers()
+    // Update is called once per frame
+    void Update()
     {
-        for (int i = 0; i < QueueSize; i++)
+        if (Input.GetKeyDown(KeyCode.U))
         {
-            
-            //followers.Add();
+            CycleFollowers(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            CycleFollowers(false);
+        }
+
+    }
+
+    private void InitializeFollowers()
+    {
+        int i = 0;
+        foreach (FollowerSlot slot in FollowerList)
+        {
+            slot.CurrentPosition = i++;
+        }
+
+        SetFollowerPositions();
+
+    }
+
+    private void SetFollowerPositions()
+    {
+        Debug.Log("Setting Positions");
+        for (int i = 0; i < FollowerList.Count; i++)
+        {
+            FollowerList[i].VectorOffset = SetPosition(FollowerList[i].CurrentPosition);
         }
     }
+
+    private Vector3 SetPosition(int position)
+    {
+        float xPos, zPos;
+        xPos = zPos = 0;
+        switch (currentQueueType)
+        {
+            case QueueTypes.LINE:
+                zPos = (position + 1) * LineZedOffset;
+                break;
+
+            case QueueTypes.BARRIER_FRONT:
+
+                break;
+            case QueueTypes.CIRCLE:
+
+                break;
+
+            case QueueTypes.PHALANX:
+
+                break;
+
+            default:
+                Debug.Log("UNDEFINED QUEUE TYPE: " + currentQueueType.ToString());
+                throw new SystemException();
+                break;
+        }
+
+        return new Vector3(xPos, 0, zPos);
+    }
+
+    public void FillerGone(int followerPosition)
+    {
+        Debug.Log("The Filler's Gone, away.");
+    }
+
+    public void Push(GameObject filler)
+    {
+        CycleFollowers(true);
+        GetFirstFollower().Filler = filler;
+
+    }
+
+    public GameObject Pop()
+    {
+        GameObject filler = GetFirstFollower().Filler;
+        GetFirstFollower().PopFiller();
+        return filler;
+    }
+
+
+    private FollowerSlot GetFirstFollower()
+    {
+        foreach (FollowerSlot slot in FollowerList)
+        {
+            if (slot.CurrentPosition == 0)
+            {
+                return slot;
+            }
+        }
+
+        return null;
+    }
+
+    
+
+    private void SetUpQueueStuff()
+    {
+        sumQueueTypes = Enum.GetNames(typeof(QueueTypes)).Length - 1;
+        
+        lastQueueType = (QueueTypes)Enum.GetNames(typeof(QueueTypes)).Length - 1;
+        numOccupiedSlots = FollowerList.Count;
+    }
+
+    public void CycleQueueType()
+    {
+        
+
+        Debug.Log("Current queue type: " + currentQueueType.ToString());
+        if (currentQueueType >= lastQueueType)
+        {
+            currentQueueType = (QueueTypes) 0;
+        }
+        else
+        {
+            currentQueueType++;
+        }
+        
+        Debug.Log("New queue type: " + currentQueueType.ToString());
+    }
+
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
 
     public void CycleFollowers(bool toBack)
     {
-        foreach (FollowerSlot slot in followers)
+        for (int index = 0; index < FollowerList.Count; index++)
         {
-            
+            // if we're moving back
+            if (toBack)
+            {
+                //if this is the last in line, move to front
+                if (FollowerList[index].CurrentPosition == FollowerList.Count - 1)
+                {
+                    FollowerList[index].CurrentPosition = 0;
+                }
+                else
+                {
+                    FollowerList[index].CurrentPosition++;
+                }
+                
+            }
+
+            // if we're moving forward
+            if (!toBack)
+            {
+                // if this is the first, move to the back
+                if (FollowerList[index].CurrentPosition == 0)
+                {
+                    FollowerList[index].CurrentPosition = FollowerList.Count - 1;
+                }
+                else
+                {
+                    FollowerList[index].CurrentPosition--;
+                }
+            }
         }
+
+        SetFollowerPositions();
     }
 }
